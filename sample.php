4031d35cf5c83b73553329989f076be26a8e4964
dhiraj-gupta <dhirajkumar.gupta@pfizer.com>
2017-09-12 15:03:05 +0530

    updated sample.php file

--- sample.php
+++ sample.php
@@ -6,4 +6,10 @@ print_r($_SERVER['nokey']);
 
 // daskhdkhsadkhakdhakhsdkahsdkhasdjaldmwqldmlmdlmasldmsalmdalsmdlaksmdlsamdlamsdlmasldmaldmalsmdlasmdlsamdlamsdlamsdlmasldmasldmalsdmalsmdlasmdlasmdlasmdlmasldmaldmad
 
+function run() {
+  prepare('action1');                              // Non-Compliant - 'action1' is duplicated 3 times
+  execute('action1');
+  release('action1');
+}
+
 ?>
