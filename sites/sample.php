7fd947af33061af337a64c2e8594736a22928631
dhiraj-gupta <dhirajkumar.gupta@pfizer.com>
2017-09-12 15:53:24 +0530

    updated in sites folder

--- sites/sample.php
+++ sites/sample.php
@@ -6,6 +6,11 @@ print_r($_SERVER['nokey']);
 
 // daskhdkhsadkhakdhakhsdkahsdkhasdjaldmwqldmlmdlmasldmsalmdalsmdlaksmdlsamdlamsdlmasldmaldmalsmdlasmdlsamdlamsdlamsdlmasldmasldmalsdmalsmdlasmdlasmdlasmdlmasldmaldmad
 
+
+$have_time = true;
+$have_money = false;
+$take_vacation = $have_time and $have_money;
+
 function run() {
   prepare('action1');                              // Non-Compliant - 'action1' is duplicated 3 times
   execute('action1');
